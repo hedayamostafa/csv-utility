dc777bdcf035ee6b869973efd7235d89edb78c9e
hedaya Mostafa <hedayaselim1807@gmail.com>
2023-10-17 01:35:05 +0200

    remove unnecessary code

--- src/main/java/com/example/rest/controller/FileUploadController.java
+++ src/main/java/com/example/rest/controller/FileUploadController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Slf4j
